46cbc6a07cc8ac3d5e263e5c3a3a04859f95bc75
y-song <y.song@berkeley.edu>
2025-12-31 20:20:52 -0800

    Running hadd during jobs

--- pyjetty/alice_analysis/slurm/youqi/process_embed_ENC.sh
+++ pyjetty/alice_analysis/slurm/youqi/process_embed_ENC.sh
@@ -4,7 +4,6 @@
 # process the input file and write an output ROOT file.
 # The main use is to give this script to a slurm script.
 
-# Take two command line arguments -- (1) input file path, (2) output dir prefix
 if [ "$1" != "" ]; then
   INPUT_FILE=$1
   echo "Input file: $INPUT_FILE"
@@ -44,16 +43,13 @@ fi
 # Define output path from relevant sub-path of input file
 OUTPUT_PREFIX="AnalysisResults/youqi/$JOB_ID"
 # Note: suffix depends on file structure of input file -- need to edit appropriately for each dataset
-OUTPUT_SUFFIX=$(echo $INPUT_FILE | cut -d/ -f11-15)
+NUMBER="${INPUT_FILE#*combined}"
+OUTPUT_SUFFIX="${NUMBER%.root}"
+# OUTPUT_SUFFIX=$(echo $INPUT_FILE | cut -d/ -f11-15)
 OUTPUT_DIR="/global/cfs/projectdirs/alice/alicepro/hiccup/rstorage/alice/$OUTPUT_PREFIX/$OUTPUT_SUFFIX"
 echo "Output dir: $OUTPUT_DIR"
 mkdir -p $OUTPUT_DIR
 
-# Load modules
-BASR_DIR="/global/cfs/cdirs/alice/youqi/mypyjetty"
-cd $BASR_DIR
-source /global/cfs/cdirs/alice/youqi/pyjetty_env.sh
-
 # Run python script via pipenv
-cd ${BASR_DIR}/pyjetty/pyjetty/alice_analysis
-python /global/cfs/cdirs/alice/youqi/mypyjetty/pyjetty/pyjetty/alice_analysis/process/user/youqi/process_embed_analyze_all_combined_jets.py -c /global/cfs/cdirs/alice/youqi/mypyjetty/pyjetty/pyjetty/alice_analysis/config/ENC/PbPb/process_data.yaml -f $INPUT_FILE -o $OUTPUT_DIR -fmc $INPUT_FILE_MC
+# cd ${BASR_DIR}/pyjetty/pyjetty/alice_analysis
+python /global/cfs/cdirs/alice/youqi/mypyjetty/pyjetty/pyjetty/alice_analysis/process/user/youqi/mixed_event_embed.py -c /global/cfs/cdirs/alice/youqi/mypyjetty/pyjetty/pyjetty/alice_analysis/config/ENC/PbPb/process_data.yaml -f $INPUT_FILE -o $OUTPUT_DIR -fmc $INPUT_FILE_MC
--- pyjetty/alice_analysis/slurm/youqi/submit.sh
+++ pyjetty/alice_analysis/slurm/youqi/submit.sh
@@ -5,16 +5,17 @@
 #SBATCH --account=alice
 #SBATCH --job-name=youqi
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
-#SBATCH --time=4:00:00
-#SBATCH --array=1-99
+#SBATCH --time=4:30:00
+#SBATCH --array=1-499
 #SBATCH --output=/global/cfs/projectdirs/alice/alicepro/hiccup/rstorage/alice/AnalysisResults/youqi/slurm-%A_%a.out
+#SBATCH --mem=60G
 
-FILE_PATHS='/global/cfs/cdirs/alice/youqi/lists/files_LHC18qr.txt'
+INPUT_LIST='/global/cfs/cdirs/alice/youqi/lists/files_LHC18qr.txt'
 FILE_PATHS_MC='/global/cfs/cdirs/alice/youqi/lists/files_LHC20g4_568_pthat28.txt'
 NFILES=$(wc -l < $FILE_PATHS_MC)
 echo "N files to process: ${NFILES}"
 
-FILES_PER_JOB=$(( $NFILES / 99 + 1 ))
+FILES_PER_JOB=$(( $NFILES / 499 + 1 ))
 echo "Files per job: $FILES_PER_JOB"
 echo "SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
 
@@ -31,10 +32,20 @@ echo "STOP = $STOP"
 
 for (( JOB_N = $START; JOB_N <= $STOP; JOB_N++ ))
 do
-  FILE=$(sed -n "$JOB_N"p $FILE_PATHS)
+  # Load modules
+  source /global/cfs/cdirs/alice/youqi/pyjetty_env.sh
+  
+  # Create combined input file by hadding together 200 files
+  FILE_COMBINED_LIST=$(shuf -n 200 $INPUT_LIST | tr '\n' ' ')
+  FILE_DATA=/pscratch/sd/y/youqi/combined$JOB_N.root
+  hadd -f $FILE_DATA $FILE_COMBINED_LIST 2>&1 | grep -v "TBufferFile::ReadObject" | grep -v "TList::Merge"
+  # hadd -v 0 $FILE_DATA $FILE_COMBINED_LIST
+  
   FILE_MC=$(sed -n "$JOB_N"p $FILE_PATHS_MC)
-  srun process_embed_ENC.sh $FILE $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID $JOB_N $FILE_MC
-  # srun pythia_gen_embed_ENC.sh $FILE $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID $JOB_N
+  srun /global/cfs/cdirs/alice/youqi/mypyjetty/pyjetty/pyjetty/alice_analysis/slurm/youqi/process_embed_ENC.sh $FILE_DATA $SLURM_ARRAY_JOB_ID $SLURM_ARRAY_TASK_ID $JOB_N $FILE_MC
+  
+  # Remove combined input file
+  rm $FILE_DATA
 done
 
 mkdir -p /global/cfs/projectdirs/alice/alicepro/hiccup/rstorage/alice/AnalysisResults/youqi/${SLURM_ARRAY_JOB_ID}/slurm_output 
